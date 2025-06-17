01b606480ea61c41b934c8c7695951f5a6a9edcb
Harikakollipara <harikakollipara18@gmail.com>
2025-06-17 14:24:40 +0530

    Add files via upload

---
+++ operators/logical.java
@@ -0,0 +1,11 @@
+class Logical
+//AND,OR,NOT
+{
+public static void main(String[] args)
+{
+int a=10,b=10;
+System.out.println(a<3 && b>1);
+System.out.println(a<3 || b>1);
+System.out.println(a==b);
+}
+}
